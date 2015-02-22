12423b8d33297cb9d52d10c7a996b27a629bd805
Suraj Adhikari <surajadh@outlook.com>
2015-02-22 09:17:11 -0500

    #20 #19 Add aboutus and contactus page

--- site/src/main/webapp/WEB-INF/scripts/app.js
+++ site/src/main/webapp/WEB-INF/scripts/app.js
@@ -4,6 +4,8 @@ myhh.config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/lesson-plans', {templateUrl: '/static/partials/lesson-plans.html', controller: 'lessonPlanController'})
     .when('/home', {templateUrl: '/static/partials/home.html'})
+    .when('/ContactUs', {templateUrl: '/static/partials/ContactUs.html'})
+    .when('/AboutUs', {templateUrl: '/static/partials/AboutUs.html'})
     .when('/', {templateUrl: '/static/partials/home.html'})
     .otherwise({redirectTo: '/'});
 }]);
